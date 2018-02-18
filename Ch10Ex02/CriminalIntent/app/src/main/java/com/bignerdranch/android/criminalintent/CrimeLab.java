7b253092c5596c2eae641d86993a16f1460f70b4
Przemysław Moskal <ptmoskal@gmail.com>
2018-02-18 21:19:09 +0100

    Add Chapter 10 Exercise 02

---
+++ Ch10Ex02/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,39 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private Map<UUID, Crime> mCrimes;
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new LinkedHashMap<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.put(crime.getId(), crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return new ArrayList<>(mCrimes.values());
+    }
+
+    public Crime getCrime(UUID id) {
+       return mCrimes.get(id);
+    }
+}
+
